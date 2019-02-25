ff1ce6c3ab3fa62d10714ca1a10752d0cf3b9cce
Phazonfish <phazonfish@gmail.com>
2019-02-24 23:51:37 -0500

    Comments Update 6

--- CS340 Final Python/Final_Update.py
+++ CS340 Final Python/Final_Update.py
@@ -3,20 +3,24 @@ import sys
 import pymongo
 from bson import json_util
 from pymongo import MongoClient
+
+//Connect to the database and collection
 connection = MongoClient('localhost', 27017)
 db = connection['market']
 collection = db['stocks']
+
+//function for section II C (see rubric)
 def update_document():
   targetTicker = sys.argv[1]
   newVolume = sys.argv[2]
   if newVolume > 0:
     try:
       result = collection.update_many({"Ticker" : targetTicker}, {'$set' : {"Volume" : newVolume}})
-    except Exception as ve:
+    except Exception as ve:         //generic exception handling, add specific exception possibilities when possible
       return ve
   else:
     result = "Please enter a positive value."
   return result
 def main():
-  print update_document()
+  print update_document()           //print results
 main()
